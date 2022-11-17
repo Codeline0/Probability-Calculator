f5a61b12c8d05dd62d02219c26a33b6e8162a827
tonycc <tonycamezcha@gmail.com>
2022-11-17 13:37:53 -0600

    feat: finish Hat class

--- prob_calculator.py
+++ prob_calculator.py
@@ -2,7 +2,19 @@ import copy
 import random
 
 class Hat:
-    pass
+    def __init__(self, **color) -> None:
+        self.color = color
+        self.contents = [key for key, val in self.color.items() for num in range(val)]
+
+    def draw(self, num_balls):
+        if num_balls > len(self.contents):
+            return self.contents
+
+        draw_balls = []
+        for num in range(num_balls):
+            rand_idx = random.randrange(len(self.contents))
+            draw_balls.append(self.contents.pop(rand_idx))
+        return draw_balls
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     pass
